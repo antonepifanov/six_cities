53e1dc74487f706f23d1c73f440696df8cbeaf9a
antonepifanov <antonepifanov87@gmail.com>
2023-03-21 17:39:03 +0300

    Добавляет компоненты главного экрана и карточки

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from '../main-page/main-page';
+
+const App = ({placeCards}) => {
+  return (
+    <MainPage
+      placeCards={placeCards}
+    />
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {PLACE_CARDS} from './mock-data';
+import App from './components/app/app';
 
+ReactDOM.render(
+    <App
+      placeCards={PLACE_CARDS}
+    />,
+    document.querySelector(`#root`)
+);
